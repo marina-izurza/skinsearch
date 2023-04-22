45766ddb9d6b3efb83a1427dd987bdb26a0e07ac
Marina <77213908+marina-izurza@users.noreply.github.com>
2023-04-22 12:32:12 +0200

    Funcionalidad POP-UP finalizada

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+    //Verifica si el pop up ya se mostró
+    var popupShown = localStorage.getItem('popupShown');
 
+    if (!popupShown) {
+        setTimeout(function () {
+            // Crea el HTML para el pop up
+            var html = '<div class="popup">' +
+                '<button class="close-btn">X</button>' +
+                '<h2>¡Bienvenido!</h2>' +
+                '<p>Por favor inicia sesión o regístrate para acceder a todo el contenido.</p>' +
+                '<button class="login-btn">Iniciar sesión</button>' +
+                '<button class="register-btn">Registrarse</button>' +
+                '</div>';
 
-})
+            // Agrega el pop up al final del body
+            $('body').append(html);
+
+            // Agrega eventos de click para los botones de login y registro
+            $('.login-btn').click(function () {
+                window.location.href = "iniciar.php";
+            });
+
+            // Agrega eventos de click para los botones de login y registro
+            $('.register-btn').click(function () {
+                window.location.href = "registro.php";
+            });
+
+            //Evento para cerrar el pop up
+            $('.close-btn').click(function () {
+                $('.popup').hide();
+            })
+
+        }, 1000); // 60000 milisegundos (1 minuto)
+
+        //Almacena un valor en localStorage para indicar que el pop up ya salió
+        localStorage.setItem('popupShown', true);
+    }
+
+});
--- view/footer.php
+++ view/footer.php
@@ -4,9 +4,9 @@
     <p class="text-secondary text-center">© SkinSearch. 2023</p>
 </footer>
 
-<script src="js/scripts.js"></script>
 <!--jQuery-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="js/scripts.js"></script>
 
 </body>
 
